06cea851ff132b2a5756ede49ef68f92fd20db69
Sublimee <aquariusborelias@gmail.com>
2019-09-28 12:40:10 +0300

    init commit

---
+++ photoappapiusers/src/main/java/com/cloud/photoapp/api/users/PhotoappapiusersApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.photoapp.api.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoappapiusersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoappapiusersApplication.class, args);
+    }
+
+}
---
+++ photoappdicsoveryservice/src/test/java/com/cloud/photoapp/dicsovery/PhotoAppDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.photoapp.dicsovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppDiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
