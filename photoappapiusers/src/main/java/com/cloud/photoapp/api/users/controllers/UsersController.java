06090cfc89e3bfb403ed51ad95f5b622510bf583
Sublimee <aquariusborelias@gmail.com>
2019-10-03 21:59:58 +0300

    check Users microservice parallel run

--- photoappapiusers/src/main/java/com/cloud/photoapp/api/users/controllers/UsersController.java
+++ photoappapiusers/src/main/java/com/cloud/photoapp/api/users/controllers/UsersController.java
@@ -1,16 +1,22 @@
 package com.cloud.photoapp.api.users.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequestMapping("/users")
 public class UsersController {
 
+    @Autowired
+    private Environment environment;
+
     @GetMapping("/status/check")
-    public String status(){
-        return "Working ...";
+    public String status() {
+        return environment.getProperty("local.server.port");
     }
 
 }
